b9a1e1b2978d7b41743c3dc3038b6ca5a5422d2b
lille <lille@MEOW>
2023-09-06 14:18:21 +0200

    Add project files.

---
+++ demodemo420blazeit/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace demodemo420blazeit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, andreas!");
+            
+        }
+    }
+}
